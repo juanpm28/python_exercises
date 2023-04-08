bf77133f5eff1a516957324e9227aca67dfc17eb
Juan de Jesús Patricio Muñiz <juan.patriciom28@gmail.com>
2023-04-08 15:13:35 -0500

    Se completó ejercicio 5

---
+++ python files/exercise5.py
@@ -0,0 +1,15 @@
+def first_last(numbers):
+  
+  number_i = numbers[0]
+  number_f = numbers[len(numbers) - 1]
+  
+  if number_i == number_f:
+    return True
+  else:
+    return False
+
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+
+print("result is" + " " + str(first_last(numbers_x)))
+print("result is" + " " + str(first_last(numbers_y)))
--- python files/tempCodeRunnerFile.py
+++ python files/tempCodeRunnerFile.py
@@ -1,5 +1,15 @@
-def remove_chars(word, size):
-  new_word = word[:size]
-  return new_word
+def first_last(numbers):
+  
+  number_i = numbers[0]
+  number_f = numbers[len(numbers) - 1]
+  
+  if number_i == number_f:
+    return True
+  else:
+    return False
 
-remove_chars("juanadssa", 2)
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+
+print("result is", str(first_last(numbers_x)))
+print("result is"+str(2))
